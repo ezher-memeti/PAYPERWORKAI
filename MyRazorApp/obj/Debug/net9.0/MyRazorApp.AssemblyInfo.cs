4587882dc49c99d7d1b4552655464d7a72a2c6e6
Ezher Memeti <ezher.memeti@gmail.com>
2025-02-15 20:36:08 +0100

    update on design

--- MyRazorApp/obj/Debug/net9.0/MyRazorApp.AssemblyInfo.cs
+++ MyRazorApp/obj/Debug/net9.0/MyRazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57de0fce89bab88541090b6aad138cc36cfff512")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a77447f17acdc3fc1f6d21c217de4fd10229fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
