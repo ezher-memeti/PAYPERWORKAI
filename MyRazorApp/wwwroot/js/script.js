64475f79100c1ec63a6feb6f76147e234331c4e0
Ezher Memeti <ezher.memeti@gmail.com>
2025-02-17 00:46:22 +0100

    Revert "update"
    This reverts commit b22c77a3eae15fe306c838e60d8a5b492c61f456.

--- MyRazorApp/wwwroot/js/script.js
+++ MyRazorApp/wwwroot/js/script.js
@@ -78,7 +78,6 @@ document.addEventListener("DOMContentLoaded", function () {
             if (video) {
                 video.play();
                 video.style.opacity = "1";
-                video.parentElement.classList.remove("paused");
                 extractGlowColor(video, ctx, canvas, button);
             }
             selectText.style.opacity = "1"; // Show "Select"
@@ -91,7 +90,6 @@ document.addEventListener("DOMContentLoaded", function () {
             if (video) {
                 video.pause();
                 video.currentTime = 0;
-                video.parentElement.classList.add("paused");
                 button.style.boxShadow = "none";
                 video.style.opacity = 0.6;
             }
