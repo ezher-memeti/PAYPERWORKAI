1d1d19384baaa6901a0064f3f3b44c681c317849
Ezher Memeti <ezher.memeti@gmail.com>
2025-02-17 00:55:29 +0100

    bug fixed

--- MyRazorApp/obj/Debug/net9.0/MyRazorApp.AssemblyInfo.cs
+++ MyRazorApp/obj/Debug/net9.0/MyRazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a152d58a634c6e8103599296561dc831e1d0e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e9aebe20f2dff05e56d336015e53c9027cf164")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
