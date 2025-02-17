add1edd680adfeb63df3ed736a2fbd7153f4ee82
Yasir Aktaş <yasiraktas28@gmail.com>
2025-02-18 02:52:45 +0300

    LoginandRegister

--- MyRazorApp/MyRazorApp.UI/obj/Debug/net9.0/MyRazorApp.UI.AssemblyInfo.cs
+++ MyRazorApp/MyRazorApp.UI/obj/Debug/net9.0/MyRazorApp.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c6c7f23aefef1b8f15a19ec3df621796c989083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2059deb4587c247834b2c27c8489a1967c9f5148")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
