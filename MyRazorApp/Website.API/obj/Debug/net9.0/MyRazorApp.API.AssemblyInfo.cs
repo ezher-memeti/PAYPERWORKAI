db32760119d0d3af65e9cc1b913264814cdaac85
Yasir Aktaş <yasiraktas28@gmail.com>
2025-03-04 23:02:07 +0300

    öenmsiz

--- MyRazorApp/Website.API/obj/Debug/net9.0/MyRazorApp.API.AssemblyInfo.cs
+++ MyRazorApp/Website.API/obj/Debug/net9.0/MyRazorApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a98d84ea8b195d40f633487e3545334a4a7f21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f016db570a02de54022980d2c98d7db8c84e7048")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
