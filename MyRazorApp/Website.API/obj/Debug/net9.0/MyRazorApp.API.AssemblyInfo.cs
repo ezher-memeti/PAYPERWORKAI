7a295ec45bbfc2a3c6ae7b775905eae276254eea
Ezher Memeti <ezher.memeti@gmail.com>
2025-03-09 23:42:08 +0300

    update

--- MyRazorApp/Website.API/obj/Debug/net9.0/MyRazorApp.API.AssemblyInfo.cs
+++ MyRazorApp/Website.API/obj/Debug/net9.0/MyRazorApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb6d812ec9943325cba8712c7b09afba916ab30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8ab864757af2ef8b4a15a8542d0135a63748490")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MyRazorApp/Website.API/obj/Release/net9.0/MyRazorApp.API.AssemblyInfo.cs
+++ MyRazorApp/Website.API/obj/Release/net9.0/MyRazorApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f98367418711f7461d13058bc43c78ec9e8461f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8ab864757af2ef8b4a15a8542d0135a63748490")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
